8acaced25de44e223afbd3efdd4d9da53bcc8254
yumyumcurryman <micahkelias@gmail.com>
2022-11-09 11:57:21 -0500

    removed vars

--- src/core/room.ts
+++ src/core/room.ts
@@ -3,8 +3,6 @@ import { API_PREFIX } from './socket'
 
 
 export async function createRoom() {
-    console.log('printing env')
-    console.log(`${API_PREFIX}/create-room`)
     try {
         const response = await axios.get(`${API_PREFIX}/create-room`)
         return response.data
