f8e175f5ac113f6cd3afa20905e15329fdb745bd
yumyumcurryman <micahkelias@gmail.com>
2022-11-14 18:21:38 -0500

    added favicon

--- src/core/socket.ts
+++ src/core/socket.ts
@@ -1,25 +1,22 @@
 import { io } from "socket.io-client";
 
-
 export const API_PREFIX = process.env.NEXT_PUBLIC_API_URL as string;
 
 export const socket = io(API_PREFIX, {
-    autoConnect: false,
-})
-
+  autoConnect: false,
+});
 
 export function joinRoom(room: string, name: string) {
-    socket.auth = {
-        room: room,
-        username: name
-    }
-    if (!socket.active) {
-        socket.connect()
-    }
+  socket.auth = {
+    room: room,
+    username: name,
+  };
+  if (!socket.active) {
+    socket.connect();
+  }
 }
 
-
 export function updateLiveDoc(doc: string) {
-    console.log('sending update')
-    socket.emit('update-doc', doc)
+  console.log("sending update");
+  socket.emit("update-doc", doc);
 }
